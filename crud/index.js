60d45a287f83f175cfb93df7b890467f8a290a6d
Atharva <atharvassenapati@gmail.com>
2024-05-08 22:59:44 +0530

    changes done

--- crud/index.js
+++ crud/index.js
@@ -3,28 +3,28 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 //get api
-app.get('/',async(req,res)=>{
+app.get('/getData/',async(req,res)=>{
 let result = await dbConnect();
 result = await result.find().toArray();
 res.send(result);
 })
 
 //post api
-app.post('/', async (req, res) => {
+app.post('/postData/', async (req, res) => {
     let result = await dbConnect();
     result = await result.insertOne(req.body);
     res.send("Data inserted Successfully");
   });
   
   //put api
-  app.put('/:name', async (req, res) => {
-    let result = await dbConnect();
-    result = await result.updateOne({name:req.params.name},{$set:req.body});
-    res.send("Data updated Successfully");
+  app.put('/putData/:name', async (req, res) => {
+  let result = await dbConnect();
+  result = await result.updateOne({name:req.params.name},{$set:req.body});
+  res.send("Data updated Successfully");
   });
 
   //delete api
-  app.delete('/:name', async (req, res) => {
+  app.delete('/deleteData/:name', async (req, res) => {
     let result = await dbConnect();
     result = await result.deleteOne({name:req.params.name});
     res.send("Data deleted Successfully");
--- docker/2B/atharva.js
+++ docker/2B/atharva.js
@@ -1,4 +1,2 @@
 console.log("Atharva Senapati");
 console.log("Third Year IT");
-console.log("Sanika")
-console.log("Aishwarya")
